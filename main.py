5c5c15ebbd768d7a80184055f581540f432eec9d
andrew <booleanorama@gmail.com>
2024-01-19 18:56:49 -0500

    Secure key

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import os
 
 load_dotenv()
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
